7fc56515fe702e15a2eec724098a6617b85174d5
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-18 23:06:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,6 @@ def assign_rooms(speakers)
   speakers.each_with_index{|speaker,index|
     assign.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")}
   end
-  assign
 end
 
 def printer(attendees)
