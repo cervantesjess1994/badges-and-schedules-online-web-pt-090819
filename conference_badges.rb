679b558ecff3084cb605270b07ad7c9954cba843
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-18 21:47:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
@@ -7,7 +7,7 @@ def batch_badge_creator(array)
       array.each do |name|
         array.push("Hello, my name is #{name}.")
     end
-    return array
+    puts array
 end
 
 def assign_rooms(speakers)
