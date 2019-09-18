2c4065f26bf5997e5ee0c3df030c8138c45dd940
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-18 22:19:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,10 +18,10 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
-    puts badge
+    return badge
   end
 
   assign_rooms(attendees).each do |badge|
-    puts badge
+    return badge
   end
 end
