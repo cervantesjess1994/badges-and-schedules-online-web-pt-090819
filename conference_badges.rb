7c11b403921893c7f6d0dd1f8bc6a1d56b8d6863
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-18 21:42:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,10 +12,10 @@ def batch_badge_creator(array)
     return array
 end
 
-def assign_rooms(name)
-  name.each_with_index{|name,i| "Hello #{name}, you'll be assigned to room #{i}!"}
-end
-  assign_rooms
+def assign_rooms(speakers)
+  assign=[]
+  speakers.each_with_index{|speaker,index| assign.push("Hello #{speaker}, you'll be assigned to room #{index +1}!")}
+  return assign 
 end
 
 def printer(assign_rooms)
