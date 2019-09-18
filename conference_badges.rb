84233ef88a4933e6cedb7eebe36cb91ea9422c3b
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-18 21:35:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+people=["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+      array.new
+      array.each do |name|
+        array.push("Hello, my name is #{name}.")
+    end
+    return array
+end
+
+def assign_rooms(name)
+  name.each_with_index{|name,i| "Hello #{name}, you'll be assigned to room #{i}!"}
+end
+  assign_rooms
+end
+
+def printer(assign_rooms)
+  puts batch_badge_creator <<-("#{assign_rooms}")
+end
